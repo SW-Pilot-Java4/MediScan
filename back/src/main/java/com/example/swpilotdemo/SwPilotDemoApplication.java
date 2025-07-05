26ca585caebad6c6246cb860c639172d2263a5e0
jae9380 <ljy5314@gmail.com>
2025-07-05 10:55:41 +0900

    setting

---
+++ back/src/main/java/com/example/swpilotdemo/SwPilotDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.swpilotdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwPilotDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwPilotDemoApplication.class, args);
+    }
+
+}
---
+++ back/src/test/java/com/example/swpilotdemo/SwPilotDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swpilotdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwPilotDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
