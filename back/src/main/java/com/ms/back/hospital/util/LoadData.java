ebb1583036329617f5ae90a629e2690f7e335fd4
jae9380 <ljy5314@gmail.com>
2025-07-06 14:52:52 +0900

    feat : 병원 파일 데이터를 사용하여 데이터 저장
    - Apache Commons CSV 추가
    파일 데이터를 읽을 때 쉼표를 구분자로 사용하여 배열을 만들 때 주소 부분에 포함되어 있는 쉼표 때문에 발생되는 문제 해결을 위함
    - 데이터 베이스 저장을 위한 간단한 코드 작성
    
    Resolves: #8fccd51

--- back/src/main/java/com/ms/back/hospital/util/LoadData.java
+++ back/src/main/java/com/ms/back/hospital/util/LoadData.java
@@ -1,7 +1,12 @@
 package com.ms.back.hospital.util;
 
+import com.ms.back.hospital.entity.Hospital;
+import com.ms.back.hospital.repository.HospitalRepository;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -9,38 +14,36 @@ import org.springframework.transaction.annotation.Transactional;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class LoadData {
+    private final HospitalRepository hospitalRepository;
 // 임시 서비스
     @Transactional
     public void readSampleData() throws Exception {
         ClassPathResource resource = new ClassPathResource("initData/sampleData.csv");
 
         try (InputStream inputStream = resource.getInputStream();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+             Reader reader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withFirstRecordAsHeader())) {
 
-            String line = reader.readLine();
+            for (CSVRecord record : csvParser) {
 
-            while ((line = reader.readLine()) != null) {
-                String[] tokens = line.split(",", -1); // 빈 문자열도 포함해서 split
+                String ykiho = record.get(0);
+                String name = record.get(1);
+                String code = record.get(2);
+                String address = record.get(10);
+                String callNumber = record.get(11);
+                String x = record.get(28);
+                String y = record.get(29);
 
-                if (tokens.length >= 32) {
-                    String ykiho = tokens[0];
-                    String name = tokens[1];
-                    String classCode = tokens[2];
-                    String address = tokens[10];
-                    String phone = tokens[11];
-                    String x = tokens[30];
-                    String y = tokens[31];
+                Hospital hospital = Hospital.create(ykiho, name, code, address, callNumber, x, y);
 
-                    System.out.println("통과 행 형식: " + line);
-                } else {
-                    System.err.println("⚠️ 잘못된 행 형식: " + line);
-                }
+                hospitalRepository.save(hospital);
             }
         }
     }
