9b605902931e25ad2e502e256681ac58109c05fc
jae9380 <145907325+jae9380@users.noreply.github.com>
2025-07-21 20:45:49 +0900

    Feature/front (#11)
    * 긴급 임시 커밋
    
    * design: 타이틀, 파비콘 설정
    
    * design : 레이아웃, 아이콘 설정
    
    * design : 레이아웃 작성
    
    * design : login

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Login from "./Login";
-import Register from "./Register";
-import Home from "./Home"; // ✅ 메인 컴포넌트 import 추가
+import Layout from "./components/Layout";
+import Login from "./pages/login/Login";
+import Register from "./pages/join/Register";
+import Home from "./pages/home/Home";
 import BigPicture from "./BigPicture";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/main" element={<Home />} /> {/* ✅ 괄호 닫기 */}
-        <Route path="/map" element={<BigPicture />} />
+        {/* Layout을 감싸는 공통 구조 */}
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/main" element={<Home />} />
+          <Route path="/map" element={<BigPicture />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ front/src/pages/home/Home.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 p-6">
+      <div className="w-full h-full bg-white rounded shadow p-4">
+          <div className="flex flex-row justify-around items-center ">
+            <div className="bg-yellow-400 p-4 rounded  text-sm">Box 1</div>
+            <div className="bg-yellow-400 p-4 rounded  text-sm">Box 1</div>
+            <div className="bg-yellow-400 p-4 rounded  text-sm">Box 1</div>
+            <div className="bg-yellow-400 p-4 rounded  text-sm">Box 1</div>
+        </div>
+        <div className="flex">
+        
+        </div>
+        <div className="flex"><a>s</a></div>
+        <div className="flex"><a>s</a></div>
+        <div className="flex"><a>s</a></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
+
+
+{/* <h1>메인 페이지</h1>
+      <p>환영합니다! 🎉</p> */}
