8406fefa07bf3363bfd09fa90e9392716dd6efe1
Juuuujuu <wldnjju@knu.ac.kr>
2025-07-25 14:55:41 +0900

    fix: 페이징 추가

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,12 +1,11 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
-import Login from "./pages/login/Login";
-import Register from "./pages/join/Register";
-import Home from "./pages/home/Home";
-import SearchSection from "./pages/search/SearchSection";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BigPicture from "./BigPicture";
-import "./SearchSection.css";
+import Layout from "./components/Layout";
+import Home from "./pages/home/Home";
+import Register from "./pages/join/Register";
+import Login from "./pages/login/Login";
+import SearchSection from "./pages/search/SearchSection";
+import "./pages/search/SearchSection.css";
 
 function App() {
   return (
@@ -14,12 +13,12 @@ function App() {
       <Routes>
           {/* Layout을 감싸는 공통 구조 */}
           <Route element={<Layout />}>
-            <Route path="/" element={<Login />} />
+            <Route path="/" element={<Home />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/main" element={<Home />} /> {/* ✅ 괄호 닫기 */}
             <Route path="/map" element={<BigPicture />} />
-              <Route path="/search" element={<SearchSection />} />
+            <Route path="/search" element={<SearchSection />} />
           </Route>
       </Routes>
     </BrowserRouter>
